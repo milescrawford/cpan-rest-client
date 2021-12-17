782c1d09d42ff5306f56895516e4e228837b6c83
Andreas K. Hüttel <dilfridge@gentoo.org>
2021-12-17 19:25:45 +0100

    Run tests via github actions
    Shamelessly adapted from Lab::Measurement

---
+++ .github/scripts/prove-test.bash
@@ -0,0 +1,28 @@
+# Stop script on first error
+set -o errexit
+
+# Prevent "Please tell me who you are" errors for certain DZIL configs
+git config --global user.name 'github-actions'
+
+# check perl version
+perl --version
+
+#
+# Install Dist::Zilla and module dependencies
+#
+
+cpanm -n Dist::Zilla
+
+# Install DZIL plugins etc if needed
+cd $GITHUB_WORKSPACE
+dzil authordeps --missing | grep -vP '[^\w:]' | xargs cpanm -n
+dzil listdeps --missing --cpanm | grep -vP '[^\w:~"\.]' | xargs cpanm -n
+
+# dependencies of module tests
+cpanm -n HTTP::Server::Simple
+
+#
+# Run tests (user tests as well as maintainer tests)
+#
+
+prove --verbose -l -s -r t xt
